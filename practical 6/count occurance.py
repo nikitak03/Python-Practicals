5c16c847e70a50da94d6c2d397f85c8c09256252
nikitak03 <103278640+nikitak03@users.noreply.github.com>
2022-07-12 22:47:12 +0530

    Add files via upload

---
+++ practical 6/count occurance.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+print("This is a python program to count occurences of all words in a sentence ")
+
+s=input("Enter a string: ")
+l=list(map(str, s.split(" ")))
+d=Counter(l)
+print(d)
+print("Number of occurences of each word in the given sentence are: ")
+for i in d:
+    print(i,d[i],sep=': ')
